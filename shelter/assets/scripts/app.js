5a9a7c84323fce6c51fc30a5d9e5b09bf9d7982c
Marina Elizarova <seagirl1110@gmail.com>
2022-04-21 12:57:03 +0300

    fix: fix closing burger-menu

--- shelter/assets/scripts/app.js
+++ shelter/assets/scripts/app.js
@@ -1,14 +1,27 @@
 // burger-menu
+const body = document.body;
 const burger = document.querySelector('[data-js="burger"]');
 const nav = document.querySelector('[data-js="nav"]');
 const overlay = document.querySelector('[data-js="overlay"]');
 
-
 burger.addEventListener('click', toggleMenu);
+
+nav.addEventListener('click', (event) => {
+    if (event.target.dataset.js === 'menu-link'){ closeMenu()};
+});
+
+overlay.addEventListener('click', closeMenu);
 
 function toggleMenu() {
     burger.classList.toggle('burger--open');
     nav.classList.toggle('nav--open');
-    overlay.classList.toggle('overlay--active')
+    overlay.classList.toggle('overlay--active');
+    body.classList.toggle('body--overflow');
+}
 
+function closeMenu() {
+    burger.classList.remove('burger--open');
+    nav.classList.remove('nav--open');
+    overlay.classList.remove('overlay--active');
+    body.classList.remove('body--overflow');
 }
