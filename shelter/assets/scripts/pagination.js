45ae5e55ee5ec3d7764b5097461aae9780fb850d
Marina Elizarova <seagirl1110@gmail.com>
2022-04-23 20:37:16 +0300

    fix: fix button pagination

--- shelter/assets/scripts/pagination.js
+++ shelter/assets/scripts/pagination.js
@@ -17,6 +17,26 @@ for (let i = 0; i < 6; i += 1) {
 
 renderPage(count);
 
+btnStart.addEventListener('click', () => {
+    count = 0;
+    renderPage(count);
+})
+
+btnPrev.addEventListener('click', () => {
+    count -= 1;
+    renderPage(count);
+})
+
+btnNext.addEventListener('click', () => {
+    count += 1;
+    renderPage(count);
+})
+
+btnFinish.addEventListener('click', () => {
+    count = petsColl.length - 1;
+    renderPage(count);
+})
+
 function shuffle(array) {
     const result = [...array];
     result.sort(() => Math.random() - 0.5);
@@ -24,10 +44,28 @@ function shuffle(array) {
 }
 
 function renderPage(count) {
+    if (count === 0) {
+        btnStart.setAttribute("disabled", "disabled");
+        btnPrev.setAttribute("disabled", "disabled");
+        btnNext.removeAttribute("disabled", "disabled");
+        btnFinish.removeAttribute("disabled", "disabled");
+    } else if (count === petsColl.length - 1) {
+        btnStart.removeAttribute("disabled", "disabled");
+        btnPrev.removeAttribute("disabled", "disabled");
+        btnNext.setAttribute("disabled", "disabled");
+        btnFinish.setAttribute("disabled", "disabled");
+    } else {
+        btnStart.removeAttribute("disabled", "disabled");
+        btnPrev.removeAttribute("disabled", "disabled");
+        btnNext.removeAttribute("disabled", "disabled");
+        btnFinish.removeAttribute("disabled", "disabled");
+    }
     page.innerHTML = "";
     const arr = petsColl[count];
     for (let i = 0; i < arr.length; i += 1) {
         const card = renderCard(arr[i]);
         page.appendChild(card)
     }
+    pageNum.innerHTML = `${count + 1}`;
+
 }
