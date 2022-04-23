41ceadcf50d9137b4c6972179c0041402e69c76c
Marina Elizarova <seagirl1110@gmail.com>
2022-04-23 23:35:37 +0300

    fix: fix pagination

--- shelter/assets/scripts/pagination.js
+++ shelter/assets/scripts/pagination.js
@@ -10,9 +10,76 @@ const btnFinish = document.querySelector('[data-pagination="btn-finish"]');
 
 let count = 0;
 
-const petsColl = [];
-for (let i = 0; i < 6; i += 1) {
-    petsColl.push(shuffle(pets));
+let petsColl = [];
+
+const coll = [];
+while (coll.length < 48) {
+    const item = pets[Math.floor(Math.random() * 8)];
+    if (coll.length < 8) {
+        if (!coll.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 12) {
+        const arr = coll.slice(6);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 16) {
+        const arr = coll.slice(8);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 18) {
+        const arr = coll.slice(12);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 24) {
+        const arr = coll.slice(16);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 32) {
+        const arr = coll.slice(24);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 36) {
+        const arr = coll.slice(30);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 40) {
+        const arr = coll.slice(32);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 42) {
+        const arr = coll.slice(36);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    } else if (coll.length < 48) {
+        const arr = coll.slice(40);
+        if (!arr.includes(item)) {
+            coll.push(item)
+        }
+    }
+}
+
+for (let i = 0; i < 48; i += 8) {
+    const item = coll.slice(i, i + 8);
+    petsColl.push(item);
+}
+
+console.log(petsColl)
+
+if (window.innerWidth < 768) {
+    petsColl = [];
+
+} else if (window.innerWidth < 1280) {
+    petsColl = [];
+
 }
 
 renderPage(count);
@@ -67,5 +134,4 @@ function renderPage(count) {
         page.appendChild(card)
     }
     pageNum.innerHTML = `${count + 1}`;
-
 }
