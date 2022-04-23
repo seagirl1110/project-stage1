8df4e71974e3ca0234a076871c9092d772616ad9
Marina Elizarova <seagirl1110@gmail.com>
2022-04-23 20:13:44 +0300

    feat: add render page

--- shelter/assets/scripts/pagination.js
+++ shelter/assets/scripts/pagination.js
@@ -1,10 +1,33 @@
 import pets from './pets.json' assert { type: 'json' };
 import { renderCard } from './card.js';
 
-const pages = document.querySelector('[data-js="pages"]');
+const page = document.querySelector('[data-pagination="page"]');
+const pageNum = document.querySelector('[data-pagination="page-number"]');
+const btnStart = document.querySelector('[data-pagination="btn-start"]');
+const btnPrev = document.querySelector('[data-pagination="btn-prev"]');
+const btnNext = document.querySelector('[data-pagination="btn-next"]');
+const btnFinish = document.querySelector('[data-pagination="btn-finish"]');
 
-pets.forEach(pet => {
-    const card = renderCard(pet);
+let count = 0;
 
-    pages.appendChild(card)
-})
+const petsColl = [];
+for (let i = 0; i < 6; i += 1) {
+    petsColl.push(shuffle(pets));
+}
+
+renderPage(count);
+
+function shuffle(array) {
+    const result = [...array];
+    result.sort(() => Math.random() - 0.5);
+    return result;
+}
+
+function renderPage(count) {
+    page.innerHTML = "";
+    const arr = petsColl[count];
+    for (let i = 0; i < arr.length; i += 1) {
+        const card = renderCard(arr[i]);
+        page.appendChild(card)
+    }
+}
